eb6371dea45a3a2150dde8e1d3bf246e8186dfab
evanlu1134 <evanlu1134@gmail.com>
2022-03-23 20:35:48 +0000

    q9 and q10

--- index.js
+++ index.js
@@ -74,4 +74,28 @@ function triangleArea(b, h){
 }
 console.log("Q8 ---")
 console.log(triangleArea(5, 7))
+
+function betweenTwentyAndFourty(n){
+  if (19 < n < 101) {
+    return true;
+  } else {
+    return false;
+  }
+}
+console.log("Q9 ---")
+console.log (betweenTwentyAndFourty(21))
+
+
+function largest(num1, num2, num3){
+  if (num1 > num2 && num1 > num3){
+    return num1;
+  } else if (num2 > num1 && num2 > num3){
+    return num2;
+  } else if (num3 > num1 && num3 > num2){
+    return num3;
+  }
+}
+console.log("Q10 ---")
+console.log ((largest(3, 6, 9)))
+
     
