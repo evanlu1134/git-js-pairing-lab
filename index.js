ebeb68c90bea25f91f4628c1cd5c8e89e4aea3d4
evanlu1134 <evanlu1134@gmail.com>
2022-03-23 18:03:58 +0000

    first function

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-//Code your solutions in this file
+//Q1
+function fiveToOneHundred(){
+    for (let i = 5; i <= 100; i++){
+        console.log(i)
+    }
+}
+console.log("Q1---")
+fiveToOneHundred()
